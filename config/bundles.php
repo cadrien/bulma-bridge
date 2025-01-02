585e4504168095098b78a4dc032550a69733516d
Adrien CHAUMARAT <ac@telesoft.fr>
2025-01-02 16:52:33 +0100

    remove unused dependencies

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
 ];
